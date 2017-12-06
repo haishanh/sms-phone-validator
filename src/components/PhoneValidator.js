c33d62f7dcb1c4c150b97122e379dec723c0dcf7
Haishan <hanhaishan1989@gmail.com>
2017-12-06 23:43:27 +0800

    add phone number validation

--- src/components/PhoneValidator.js
+++ src/components/PhoneValidator.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
+import PhoneNumberInput from './PhoneNumberInput';
+
 class PhoneValidator extends Component {
   render() {
     return (
       <div>
-        <input type="tel" maxLength="11" placeholder="Phone Number" />
+        <PhoneNumberInput
+          onValidationChange={s => {
+            console.log(s);
+          }}
+          {...this.props}
+        />
         <button disabled>Send</button>
       </div>
     );
   }
 }
 
+// <input type="tel" maxLength="11" placeholder={placeholder} />
 export default PhoneValidator;
