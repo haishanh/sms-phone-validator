8bd30d1f40babcca416d5056ea3c073caf235bd6
Haishan <hanhaishan1989@gmail.com>
2017-12-07 00:05:46 +0800

    impl canSend logic

--- src/components/PhoneValidator.js
+++ src/components/PhoneValidator.js
@@ -3,20 +3,29 @@ import React, { Component } from 'react';
 import PhoneNumberInput from './PhoneNumberInput';
 
 class PhoneValidator extends Component {
+  state = {
+    isPhoneNumberValid: false,
+    isVerifyInProgress: false
+  };
+
+  handleOnValidationChange = isPhoneNumberValid => {
+    this.setState({ isPhoneNumberValid });
+  };
+
   render() {
+    const { isPhoneNumberValid, isVerifyInProgress } = this.state;
+    const canSend = isPhoneNumberValid === true && isVerifyInProgress === false;
+
     return (
       <div>
         <PhoneNumberInput
-          onValidationChange={s => {
-            console.log(s);
-          }}
+          onValidationChange={this.handleOnValidationChange}
           {...this.props}
         />
-        <button disabled>Send</button>
+        <button disabled={!canSend}>Send</button>
       </div>
     );
   }
 }
 
-// <input type="tel" maxLength="11" placeholder={placeholder} />
 export default PhoneValidator;
