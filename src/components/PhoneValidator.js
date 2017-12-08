25a1f33db057ae1e29c47da34add0c9fe3930cfd
Haishan <hanhaishan1989@gmail.com>
2017-12-08 11:29:59 +0800

    using storybook for component showcase

--- src/components/PhoneValidator.js
+++ src/components/PhoneValidator.js
@@ -4,21 +4,21 @@ import PropTypes from 'prop-types';
 import PhoneNumberInput from './PhoneNumberInput';
 import SendButton from './SendButton';
 
-function fetchMockGood() {
-  const res = {
-    ok: true,
-    json: () => {}
-  };
-  return new Promise((resolve, reject) => {
-    return resolve(res);
-  });
-}
+// function fetchMockGood() {
+//   const res = {
+//     ok: true,
+//     json: () => {}
+//   };
+//   return new Promise((resolve, reject) => {
+//     return resolve(res);
+//   });
+// }
 
-function fetchMockBad() {
-  return new Promise((resolve, reject) => {
-    setTimeout(reject, 3000);
-  });
-}
+// function fetchMockBad() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(reject, 3000);
+//   });
+// }
 
 class PhoneValidator extends Component {
   static defaultProps = {
@@ -26,6 +26,8 @@ class PhoneValidator extends Component {
     buttonTextSent: '已发送',
     placeholder: '手机号',
     sendSmsBackoffTime: 60,
+    className: 'phone-validator',
+    inputName: 'phoneNumber',
     fetchImpl: fetch
   };
 
@@ -66,14 +68,15 @@ class PhoneValidator extends Component {
 
   render() {
     const { isPhoneNumberValid, isVerifyInProgress } = this.state;
-    const { buttonTextDefault, buttonTextSent } = this.props;
+    const { buttonTextDefault, buttonTextSent, className, inputName } = this.props;
     const canSend = isPhoneNumberValid === true && isVerifyInProgress === false;
     const buttonText = isVerifyInProgress ? buttonTextSent : buttonTextDefault;
 
     return (
-      <div>
+      <div className={className}>
         <PhoneNumberInput
           onValidationChange={this.handleOnValidationChange}
+          name={inputName}
           {...this.props}
         />
         <SendButton
